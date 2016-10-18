2c99b3d701b20a0d5d0e033eb7157492d0f5933c
unknown <small.box@163.com>
2016-10-18 16:52:02 +0800

    PESDExcel.py 1) Modify class private variable __xx__ to __xx 2) New search_extern_variable function
    Signed-off-by: Heyn2016 <small.box@163.com>

--- Python/PESDExcel.py
+++ Python/PESDExcel.py
@@ -17,13 +17,14 @@ from CoraExcel import CoraExcel
 class PESDExcel:
     """ESD Excel Class"""
     # write excel coln index match values list
-    colnIndex = [2, 4, 3, 5]
-    __excel_sheet_index__ = 0
-    __excel_sheet_name__ = ""
-    __excel_rown__ = 0
-    __excel_coln__ = 0
+    colnIndex = [2, 3, 5, 6]
+    __excel_sheet_index = 0
+    __excel_sheet_name = ""
+    __excel_rown = 0
+    __excel_coln = 0
     xlsInfoLists = [[] for i in range(3)]  # [[], [], []]
     excelContentDict = {}
+    externVarDict = {}
 
     def __init__(self, filepath):
         """__init__"""
@@ -33,15 +34,15 @@ class PESDExcel:
         xlsinfolists = self.excel.info()
         if xlsinfolists is None:
             raise IOError
-        self.__excel_sheet_name__ = xlsinfolists[0][self.__excel_sheet_index__]
-        self.__excel_rown__ = xlsinfolists[1][self.__excel_sheet_index__]
-        self.__excel_coln__ = xlsinfolists[2][self.__excel_sheet_index__]
+        self.__excel_sheet_name = xlsinfolists[0][self.__excel_sheet_index]
+        self.__excel_rown = xlsinfolists[1][self.__excel_sheet_index]
+        self.__excel_coln = xlsinfolists[2][self.__excel_sheet_index]
         # Read all data from excel to memory.
         dictkey = 0
 
-        for line in range(self.__excel_rown__):
+        for line in range(self.__excel_rown):
             excelrowlist = self.excel.readrow(
-                self.__excel_sheet_name__, line)
+                self.__excel_sheet_name, line)
             # Delete space " "  in excel every cell
             # excelrowlist = [(lambda val: "".join([x for x in val if x != " "]))(
             #     str(val)) for val in excelrowlist]
@@ -54,10 +55,12 @@ class PESDExcel:
     def writerow(self, messagelist, rown=0):
         """ Write excel row.
         Argument(s):
-                    messagelist :excelFileName,
-                                 functionName,
-                                 keywordsLines,
+                    messagelist :file's name,
+                                 function name,
+                                 line number,
                                  keywords
+                    i.e.
+                    ['Uart_ext.h', 'None', 110, 'extern void UART_dem_init(void);']
         Return(s):
                     None
         Notes:
@@ -70,17 +73,19 @@ class PESDExcel:
         #           (str(val)) for val in messagelist]
 
         for value in messagelist:
-            self.excel.writecell(value, self.__excel_sheet_index__,
-                                 self.__excel_rown__ + rown,
+            self.excel.writecell(value, self.__excel_sheet_index,
+                                 self.__excel_rown + rown,
                                  self.colnIndex[messagelist.index(value)])
 
     def updaterow(self, messagelist, rown=0):
         """Update excel row.
         Argument(s):
-                    messagelist :excelFileName,
-                                 functionName,
-                                 keywordsLines,
+                    messagelist :file's name,
+                                 function name,
+                                 line number,
                                  keywords
+                    i.e.
+                    ['Uart_ext.h', 'None', 110, 'extern void UART_dem_init(void);']
         Return(s):
                     exists (False)  others (True)
         Notes:
@@ -92,7 +97,7 @@ class PESDExcel:
         # messagelist = [(lambda val : "".join([x for x in val if x != ";"]))
         #           (str(val)) for val in messagelist]
 
-        for key in range(self.__excel_rown__):
+        for key in range(self.__excel_rown):
             # excelRowList and values 's Intersection
             # e.g.
             # a=[2,3,4,5]
@@ -115,7 +120,7 @@ class PESDExcel:
 
                 if len(ret) == 3:
                     # <TODO> Update keywords line number
-                    sheetn = self.__excel_sheet_index__
+                    sheetn = self.__excel_sheet_index
                     conln = self.colnIndex[messagelist.index(messagelist[2])]
                     self.excel.writecell(messagelist[2], sheetn, key, conln)
 
@@ -124,7 +129,7 @@ class PESDExcel:
                     return False
             except BaseException:
                 continue
-        print("OK")
+        print("[OK] Line = %d" % messagelist[2])
         self.writerow(messagelist, rown)
         return True
 
@@ -136,6 +141,39 @@ class PESDExcel:
             print(match.group(3), ' = ', match.group(5),
                   re.findall('\\(.*?\\)$', match.group(5)))
 
+    def search_extern_variable(self, messagelist):
+        """Search extern variable.
+        Argument(s):
+                    messagelist :file's name,
+                                 function name,
+                                 line number,
+                                 keywords
+                    i.e.
+                    ['Uart_ext.h', 'None', 110, 'extern void UART_dem_init(void);']
+        Return(s):
+                    exists (False)  others (True)
+        Notes:
+                    2016-10-18 V1.0.0[Heyn]
+        """
+
+        # Search string excluding '(' or ')' in messagelist
+        match = re.search('^((?!\\(|\\)).)*$', messagelist[3], re.I)
+        if match is not None:
+            # (1) Replace \t to ''
+            # (2) Removed ';'
+            # (3) Split string by ' '
+            # (4) Get extern variable and strip it.
+            newstr = messagelist[3].replace('\t', ' ').replace(
+                ';', '').split(' ')[-1].strip()
+            if newstr in self.externVarDict:
+                return False
+            else:
+                # File's name is messagelist[0]
+                self.externVarDict[newstr] = messagelist[0]
+                # print(newstr)
+                return True
+        return False
+
     def save(self):
         """Save excel workbook.
         Argument(s):
@@ -145,4 +183,5 @@ class PESDExcel:
         Notes:
                     2016-09-27 V1.0.0[Heyn]
         """
+
         self.excel.save()

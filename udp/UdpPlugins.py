69dc5211c865bfb7f90cf535b929a9194bf4f567
unknown <small.box@163.com>
2016-10-20 16:24:50 +0800

    V1.0.1 1) New multiprocess 2) Modify multithread class.
    Signed-off-by: Heyn2016 <small.box@163.com>

--- udp/UdpPlugins.py
+++ udp/UdpPlugins.py
@@ -7,12 +7,13 @@
 # Program:  UDP Client
 # History:  2016/10/11
 #           [PyLint Message: See web: http://pylint-messages.wikidot.com/]
+#           2016/10/20 V1.0.1 New multiprocess\Modify multithread class.
 
 import re
 import errno
 import socket
 import threading
-
+import multiprocessing
 
 IPADDR = '192.168.0.101'
 PORTNUM = 54320
@@ -23,7 +24,7 @@ PACKETDATA = b'Hello,Amp!'
 # UDP_SOCKET_BLOCKING     = 1         # No Blocking (=0), or Blocking (=1)
 UDP_SOCKET_NO_BLOCKING = 0
 
-DONGLESTRING = ''
+# DONGLESTRING = ''
 
 
 def udpconnect(server_ip='127.0.0.1'):
@@ -33,7 +34,7 @@ def udpconnect(server_ip='127.0.0.1'):
     address = (server_ip, PORTNUM)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
     sock.setblocking(UDP_SOCKET_NO_BLOCKING)
-    sock.settimeout(0.5)
+    sock.settimeout(1)
     try:
         sock.connect(address)
     except socket.error as err:
@@ -46,22 +47,45 @@ def udpconnect(server_ip='127.0.0.1'):
         sock.close()
         return ''
 
-    global DONGLESTRING     # pylint: disable=W0603
-    DONGLESTRING += dongle_message.decode() + ',' + server_ip + '##'
-
     sock.close()
+    donglestr = dongle_message.decode() + ',' + server_ip
+
+    return donglestr
 
 
 class UdpThread(threading.Thread):
     """UDP Thread."""
 
-    def __init__(self, ipaddr='127.0.0.1'):
+    def __init__(self, funclist=None):
         threading.Thread.__init__(self)
-        self.ipaddr = ipaddr
+        self.funclist = funclist
+        self.threads = []
+        self.retlist = 0
 
-    def run(self):
-        udpconnect(self.ipaddr)
+    def start(self):
+        self.retlist = []
 
+        for funcdict in self.funclist:
+            new_arg_list = []
+            new_arg_list.append(funcdict["func"])
+            for arg in funcdict["args"]:
+                new_arg_list.append(arg)
+            new_arg_tuple = tuple(new_arg_list)
+
+            ths = threading.Thread(target=self.trace_func, args=new_arg_tuple)
+            self.threads.append(ths)
+
+        for threadobj in self.threads:
+            threadobj.start()
+
+        for threadobj in self.threads:
+            threadobj.join()
+
+    def trace_func(self, func, *args, **kwargs):
+        """Thread return value"""
+        ret = func(*args, **kwargs)
+        if ret != '':
+            self.retlist.append(ret)
 
 def broadcast(broadip=('255.255.255.255', PORTNUM)):
     """ UDP BroadCast Client"""
@@ -74,25 +98,43 @@ def broadcast(broadip=('255.255.255.255', PORTNUM)):
     sockudp.settimeout(0.5)
     sockudp.bind(loacladdr)
     sockudp.sendto(PACKETDATA, broadip)
-    server_message = sockudp.recv(BUFFER_SIZE)
+    try:
+        server_message = sockudp.recv(BUFFER_SIZE)
+    except BaseException:
+        return ''
     server_message = server_message.decode() + '##'
     print(server_message)
     return server_message
 
 if __name__ == '__main__':
-    broadcast()
+    # broadcast()
 
-    THREADPOOL = []
     LOCALIP = socket.gethostbyname(socket.gethostname())
     LOCALIP = re.sub(r'[^\.][\d]{1,3}$', '', LOCALIP)
 
-    for i in range(255):
-        # print(LOCALIP + str(i))
-        th = UdpThread(LOCALIP + str(i))
-        th.start()
-        THREADPOOL.append(th)
+    RETLIST = []
+    THREADPOOL = []
 
-    for th in THREADPOOL:
-        th.join()
+    for i in range(10):
+        # THREADPOOL.append({"func":udpconnect, "args":(LOCALIP + str(i),)})
+        THREADPOOL.append({"func":udpconnect, "args":('192.168.0.101',)})
 
-    print(DONGLESTRING)
+    UDPTH = UdpThread(THREADPOOL)
+    UDPTH.start()
+    RETLIST.extend(UDPTH.retlist)
+
+    # MultiProcess Test.
+    # MAX_PROCESS = 10
+    # IPARRAY = []
+    # POOL = multiprocessing.Pool(MAX_PROCESS)
+    # for i in range(MAX_PROCESS):
+    #     # IPARRAY.append(LOCALIP + str(i))
+    #     IPARRAY.append('192.168.0.101')
+
+    # RETLIST = POOL.map(udpconnect, IPARRAY)
+    # POOL.close()
+    # POOL.join()
+
+    for item in RETLIST:
+        print(item)
+    print(len(RETLIST))

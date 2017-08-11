4ae921080aac2219279af84dab681a978552daae
unknown <small.box@163.com>
2017-08-11 15:47:37 +0800

    PBoxWebAPI new print_pretty
    Signed-off-by: Heyn2016 <small.box@163.com>

--- wheel/PBoxWebAPI/__init__.py
+++ wheel/PBoxWebAPI/__init__.py
@@ -1,5 +1,16 @@
-from .PBoxWebAPI import PBoxWebAPI
+# -*- coding:utf8 -*-
+""" PBox WebMc API """
+# !/usr/bin/python
+# Python:   3.5.2
+# Platform: Windows/ARMv7/Linux
+# Author:   Heyn (heyunhuan@gmail.com)
+# Program:  PBox WebMc API.
+# History:  2017-07-27 V1.0 [Heyn]
+#           2017-08-07 Wheel Ver:0.0.6 [Heyn]
+#
 
+from .PBoxWebAPI import PBoxWebAPI
+from .PBoxWebAPI import print_pretty
 
 instanceList = []
 
@@ -30,3 +41,5 @@ armreboot       = originInstance.reboot
 
 webnewpassword  = originInstance.newpassword
 weblanip        = originInstance.lanipaddress
+
+imageReport     = print_pretty

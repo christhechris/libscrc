9ec53388f129ca056211f8687896523b718cf11f
Heyn2016 <Small.box@163.com>
2017-08-19 22:41:12 +0800

    Modify setup.py
    Signed-off-by: Heyn2016 <Small.box@163.com>

--- libscrc/setup.py
+++ libscrc/setup.py
@@ -30,7 +30,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='libscrc',
-    version='0.0.5',
+    version='0.1.0',
 
     description='Library for calculating CRC8/CRC16/CRC32/CRC64',
     long_description=long_description,
@@ -58,8 +58,8 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',

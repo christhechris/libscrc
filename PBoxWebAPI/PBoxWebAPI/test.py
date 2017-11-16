a1f0734e66b41cf462059015f5ab8f2f524437e3
Heyn2016 <Small.box@163.com>
2017-11-16 17:00:46 +0800

    Update test.py
    Modfiy wanipaddress() and removed logging.

--- PBoxWebAPI/PBoxWebAPI/test.py
+++ PBoxWebAPI/PBoxWebAPI/test.py
@@ -1,4 +1,4 @@
-# -*- coding:utf8 -*-
+# -*- coding:utf-8 -*-
 """ Test library for PBoxWebAPI """
 # !/usr/bin/python
 # Python:   3.5.2
@@ -6,9 +6,10 @@
 # Author:   Heyn (heyunhuan@gmail.com)
 # Program:  Test library for PBoxWebAPI.
 # History:  2017-08-17 Wheel Ver:0.1.5 [Heyn] Initialize
+#           2017-11-16 Wheel Ver:1.2.1 [Heyn] Modfiy wanipaddress()
+
 
 import unittest
-import logging
 from PBoxWebAPI import PBoxWebAPI
 
 class TestPBoxWebAPI(unittest.TestCase):
@@ -18,13 +19,13 @@ class TestPBoxWebAPI(unittest.TestCase):
     def do_basics(self, module):
         """Test basic functionality.
         """
-        ret = module.login(url='https://192.168.3.222', password='Psdcd123')
+        ret = module.login(url='https://192.168.0.1', password='admin')
         self.assertEqual(ret, True)
 
-        ret = module.newpassword('Psdcd123')
+        ret = module.newpassword('admin')
         self.assertEqual(ret, True)
 
-        ret = module.confpassword('Psdcd123')
+        ret = module.confpassword('admin')
         self.assertEqual(ret, True)
 
         ret = module.newchannel(name='Test', items=['Modbus-TCP', '127.0.0.1', '51320', '5000'], flag=True)
@@ -38,8 +39,8 @@ class TestPBoxWebAPI(unittest.TestCase):
 
         ret = module.alterdevice(name='Test')
         self.assertEqual(ret, True)
-  
-        ret = module.newitem(['python', 'test', '5000', 'a', '0', '1', '1;3;2;1;INT16;0;0;0'])
+
+        ret = module.newitem(['python', 'test', '10000', 'a', '0', '1', '1;3;2;1;INT16;0;0;0'])
         self.assertEqual(ret, True)
 
         self.assertEqual(module.delitems(), True)
@@ -47,7 +48,7 @@ class TestPBoxWebAPI(unittest.TestCase):
         self.assertEqual(module.delchannel(), True)
         self.assertEqual(module.cloudaddress('47.93.79.77'), True)
         self.assertEqual(module.lanipaddress(), True)
-        self.assertEqual(module.wanipaddress(dhcp='NO'), True)
+        self.assertEqual(module.wanipaddress({'_dhcp':'NO', '_ip':'10.10.10.10', '_mask':'255.255.255.0', '_gateway':'10.10.10.1', '_dns':'8.8.8.8'}), True)
         self.assertEqual(module.netswitch(), True)
 
         print(module.version())
@@ -62,7 +63,7 @@ class TestPBoxWebAPI(unittest.TestCase):
     def test_basics(self):
         """Test basic functionality.
         """
-        self.do_basics(PBoxWebAPI(debugLevel=logging.ERROR))
+        self.do_basics(PBoxWebAPI())
 
 
 if __name__ == '__main__':
